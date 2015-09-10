06a62b510813bee61db99f4fa263c76feaed8ef7
Iago Rayner Moura <emaildoiagoo@gmail.com>
2015-09-09 22:08:45 -0300

    Movimentação para repositório independente para Deploy Contínuo em novo site do Azure. Projeto Web. Angular JS

---
+++ TrabalhoFinal.Web/app/controllers/notasController.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+app.controller('notasController', ['$scope', 'notasService', function ($scope, ordersService) {
+
+    $scope.orders = [];
+
+    notasService.getnotas().then(function (results) {
+
+        $scope.notas = results.data;
+
+    }, function (error) {
+        //alert(error.data.message);
+    });
+
+}]);
---
+++ TrabalhoFinal.Web/app/services/notasService.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+app.factory('notasService', ['$http', 'ngAuthSettings', function ($http, ngAuthSettings) {
+
+    var serviceBase = ngAuthSettings.apiServiceBaseUri;
+
+    var NotasServiceFactory = {};
+
+    var _getNotas = function () {
+
+        return $http.get(serviceBase + 'api/notas').then(function (results) {
+            return results;
+        });
+    };
+
+    ordersServiceFactory.getNotas = _getNotas;
+
+    return ordersServiceFactory;
+
+}]);
